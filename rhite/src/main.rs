24ae5983a05a829a706377e93fc6d4d968c6ec3e
MarcosA <90685475+MarcosAndradeV@users.noreply.github.com>
2024-12-05 17:16:51 -0300

    Update main.rs

--- rhite/src/main.rs
+++ rhite/src/main.rs
@@ -1,5 +1,6 @@
 use mar::MapBuilder;
 use prelude::*;
+use rand::{thread_rng, Rng};
 
 const BACKGROUND_COLOR_HEX: u32 = 0x181818FF;
 const WIDTH: usize = 800;
@@ -8,25 +9,20 @@ const BLOCK_SIZE: usize = 20;
 
 fn main() -> Result<(), ()> {
     unsafe {
-        // let map = MapBuilder::new(WIDTH, HEIGHT, BLOCK_SIZE)
-        //     .map_err(|e| eprintln!("ERROR: {e}"))?
-        //     .set_many(&[
-        //         (1, 1, colors::RED),
-        //         (2, 2, colors::BLUE),
-        //         (3, 3, colors::GREEN),
-        //     ])
-        //     .fill_vertical(0, colors::GRAY)
-        //     .fill_horizontal(0, colors::GRAY)
-        //     .apply(|mb| mb.fill_horizontal(20, colors::RED))
-        //     .fill_horizontal_strip(2, 5, 17, colors::GOLD)
-        //     .fill_vertical_strip(2, 5, 17, colors::SKYBLUE)
-        //     .build();
-        //
         let map = MapBuilder::new(WIDTH, HEIGHT, BLOCK_SIZE)
             .map_err(|e| eprintln!("ERROR: {e}"))?
-            .perlin_noise(69)
-            .build()
-        ;
+            .perlin_noise(thread_rng().gen())
+            .set_many(&[
+                (1, 1, colors::RED),
+                (2, 2, colors::BLUE),
+                (3, 3, colors::GREEN),
+            ])
+            .fill_vertical(0, colors::GRAY)
+            .fill_horizontal(0, colors::GRAY)
+            .apply(|mb| mb.fill_horizontal(20, colors::RED))
+            .fill_horizontal_strip(2, 5, 17, colors::GOLD)
+            .fill_vertical_strip(2, 5, 17, colors::SKYBLUE)
+            .build();
 
         let bg_color = GetColor(BACKGROUND_COLOR_HEX);
         let w = RlWindow::init_window(WIDTH as i32, HEIGHT as i32, rl_str!("rhite"));
