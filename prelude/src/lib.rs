726d3cdfce4dd847e0373dbf845b902b0c72189c
MarcosA <90685475+MarcosAndradeV@users.noreply.github.com>
2024-12-05 17:14:46 -0300

    Update lib.rs

--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -33,3 +33,16 @@ impl Drop for RlWindow {
         }
     }
 }
+
+pub fn color_from_hsv(hue: f32, saturation: f32, value: f32) -> Color {
+    unsafe { ColorFromHSV(hue, saturation, value) }
+}
+
+pub fn color_lerp(a: Color, b: Color, t: f64) -> Color {
+    Color {
+        r: (a.r as f64 * (1.0 - t) + b.r as f64 * t) as u8,
+        g: (a.g as f64 * (1.0 - t) + b.g as f64 * t) as u8,
+        b: (a.b as f64 * (1.0 - t) + b.b as f64 * t) as u8,
+        a: (a.a as f64 * (1.0 - t) + b.a as f64 * t) as u8,
+    }
+}
